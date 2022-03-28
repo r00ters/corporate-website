208158dd85fee0f1fb33dc6d7f00ef33e37832e6
Pasquale Fiorillo <p.fiorillo@rooters.it>
2022-03-28 16:35:55 +0200

    fix licenses; 404 layout

---
+++ src/components/Copyright.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Copyright() {
+
+    return (
+      <div className="copyright">
+        &copy; 2022 Rooters s.r.l.s. Some rights reserved. - <a href="https://github.com/r00ters/corporate-website">Source Code</a>
+      </div>
+    );
+}
+
+export default Copyright
